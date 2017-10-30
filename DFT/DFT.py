a289662bb88ddae2a4f372bc1867261f7aecd992
jasonm718 <jasonm718@gmail.com>
2017-10-30 16:15:32 -0500

    Finally solved Forward transform

--- DFT/DFT.py
+++ DFT/DFT.py
@@ -1,6 +1,8 @@
 # For this part of the assignment, please implement your own code for all computations,
 # Do not use inbuilt functions like fft from either numpy, opencv or other libraries
 
+import numpy as np
+import cmath
 
 class DFT:
 
@@ -10,10 +12,26 @@ class DFT:
         matrix: a 2d matrix
         returns a complex matrix representing fourier transform"""
 
+        """Compute the discrete Fourier Transform of the 1D array x"""
+        N = len(matrix)
+
+        darray = [[0.0 for i in range(N)] for j in range(N)]
+
+        for u in range(N):
+            for v in range(N):
+                sums = 0.0
+
+                for i in range(N):
+                    for j in range(N):
+
+                        val = matrix[i,j]
+                        e = cmath.exp(- 1j * (2.0*cmath.pi) * ((float(u * i) / N) + (float(v * j) / N)))
+                        sums += val * e
+
+                darray[u][v] = sums
 
 
-
-        return matrix
+        return darray
 
     def inverse_transform(self, matrix):
         """Computes the inverse Fourier transform of the input matrix
@@ -21,6 +39,7 @@ class DFT:
         takes as input:
         returns a complex matrix representing the inverse fourier transform"""
 
+    
 
 
         return matrix
